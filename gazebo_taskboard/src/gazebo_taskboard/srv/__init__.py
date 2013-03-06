6fdfc3532518d572835ac3db6964a83158a92a5a
Allison Thackston <allison.thackston@nasa.gov>
2013-03-06 09:33:20 -0600

    Added taskboard controller from TopCoder competition

---
+++ gazebo_taskboard/src/gazebo_taskboard/srv/__init__.py
@@ -0,0 +1,5 @@
+from ._ManipulateRockerSwitch import *
+from ._ManipulateNumPad import *
+from ._ManipulatePowerCover import *
+from ._ManipulatePowerSwitch import *
+from ._ManipulateSafeToggle import *
