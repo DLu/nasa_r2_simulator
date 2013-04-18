f7627a2c0d7a1499759cdb29a88eaca37dd4331e
Allison Thackston <allison.thackston@nasa.gov>
2013-04-18 18:46:04 -0500

    updated launch files

--- gazebo_taskboard/src/gazebo_taskboard/srv/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._ManipulateRockerSwitch import *
-from ._ManipulateNumPad import *
-from ._ManipulatePowerCover import *
-from ._ManipulatePowerSwitch import *
-from ._ManipulateSafeToggle import *
