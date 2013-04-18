a1ec39640f00839460825222f44a0880cd3e7f41
Allison Thackston <allison.thackston@nasa.gov>
2013-04-18 16:38:32 -0500

    removed unnecessary auto generated files from repo

--- gazebo_taskboard/srv_gen/lisp/_package_ManipulatePowerCover.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package gazebo_taskboard-srv)
-(cl:export '(ANGLE-VAL
-          ANGLE
-          DURATION-VAL
-          DURATION
-          OK-VAL
-          OK
-))
--- gazebo_taskboard/srv_gen/lisp/_package_ManipulateRockerSwitch.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package gazebo_taskboard-srv)
-(cl:export '(TORQUE-VAL
-          TORQUE
-          DURATION-VAL
-          DURATION
-          OK-VAL
-          OK
-))
