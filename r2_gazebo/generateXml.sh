57edb6bd39ca4e2abaa411087cec1c6f55effadb
athackst <athackst@clank.(none)>
2014-07-07 13:25:28 -0500

    updated automatic generating of urdf files

--- r2_gazebo/generateXml.sh
+++ r2_gazebo/generateXml.sh
@@ -1,8 +1,8 @@
-for file in $PWD/robots/*.xacro
+for file in `rospack find r2_gazebo`/robots/*.xacro
 do
     filename=${file%.*o}
     echo $file
     rosrun xacro xacro.py $file > "$filename.xml"
-    mv "$filename.xml" ./robots
+    mv "$filename.xml" `rospack find r2_gazebo`/robots
 done
 exit 0
